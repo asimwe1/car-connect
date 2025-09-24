a76482f4ea9f9955ce2e8154287ca46aecf05beb
Shema Leandre <iamshemaleandre@gmail.com>
2025-09-24 14:09:35 +0200

    feat(ui): global OfflineBanner wired into Layout to indicate offline state

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import FloatingActions from "@/components/FloatingActions";
+import OfflineBanner from "@/components/OfflineBanner";
 
 const Layout: React.FC = () => {
   return (
     <div className="min-h-screen bg-background flex flex-col">
+      <OfflineBanner />
       <Navbar />
       <main className="pt-16 flex-1">
         <Outlet />
